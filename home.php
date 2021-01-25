a70c6b98a4ff90071df8dd98bca5d12336e04d6f
adDigo <73916391+adDigo@users.noreply.github.com>
2021-01-25 01:00:02 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+       
+
+    </head>
+    <body>
+        <?php include('header.html')?>
+    </body>
+</html>
+
